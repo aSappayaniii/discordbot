0027b5af9a945c139a3c004ff99483e70cbb419b
aSappayaniii <trecvelocity@gmail.com>
2023-11-19 14:57:52 -0600

    added all the files

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith(".py"):
         bot.load_extension(f'cogs.{filename[:-3]}')
          
-bot.run(config.TOKEN)
+bot.run(os.environ["DISCORD_TOKEN"])
